4345919a08ff262f744db15c3c06b2ce83028718
tom2002965 <tom2002965@gmail.com>
2016-05-18 17:00:22 +0800

    Add validations to post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,21 @@
 class PostsController < ApplicationController
     def index
         @posts = Post.all.order('created_at DESC')
-        
+
     end
 
     def new
+        @post = Post.new
     end
 
     def create
         @post = Post.new(post_params)
-        @post.save
 
-        redirect_to @post
+        if @post.save
+            redirect_to @post
+        else
+            render 'new'
+        end
     end
 
     def show
